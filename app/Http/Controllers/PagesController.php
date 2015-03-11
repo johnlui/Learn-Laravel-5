872e9004a806825d513d14b7c1179c9f0ade5d05
JohnLui <wenhanlv@gmail.com>
2015-03-11 19:47:27 +0800

    去除 guest 身份认证，如果后台已登录，前台会出现奇怪的问题。

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,11 +4,6 @@ use App\Page;
 
 class PagesController extends Controller {
 
-  public function __construct()
-  {
-    $this->middleware('guest');
-  }
-
   public function show($id)
   {
     return view('pages.show')->withPage(Page::find($id));
