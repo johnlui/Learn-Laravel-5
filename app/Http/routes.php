cdd12dff195457bd30766f6c16d0cb8a174686df
JohnLui <wenhanlv@gmail.com>
2015-04-15 09:59:01 +0800

    更正拼写错误

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,7 +22,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function()
 {
-  Route::get('/', 'AdminHomeComtroller@index');
+  Route::get('/', 'AdminHomeController@index');
   Route::resource('pages', 'PagesController');
   Route::resource('comments', 'CommentsController');
 });
